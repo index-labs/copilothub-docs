babf07a572439cfda9ad73ee6ad00149142749f7
Jiang Bohan <bhjiang@outlook.com>
2023-06-17 21:37:09 +0800

    feat: add vercel analysis

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import { Analytics } from '@vercel/analytics';
 
 import styles from './index.module.css';
 
@@ -38,6 +39,7 @@ export default function Home(): JSX.Element {
          {/* <main>
             <HomepageFeatures />
          </main> */}
+         <Analytics />
       </Layout>
    );
 }
